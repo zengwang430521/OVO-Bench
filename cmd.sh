b84e2020e7557e673f4d0f53222b5e0279feb142
zengwang <zengwang@tetras.ai>
2025-01-17 14:35:46 +0800

    debug

---
+++ cmd.sh
@@ -0,0 +1,24 @@
+huggingface-cli download \
+--repo-type dataset \
+--resume-download JoeLeelyf/OVO-Bench \
+--local-dir /home/SENSETIME/zengwang/myprojects/task_define_service/data/OVO-Bench \
+--local-dir-use-symlinks False
+
+
+~/ads-cli sync \
+/home/SENSETIME/zengwang/myprojects/task_define_service/data/OVO-Bench  \
+s3://196FFD00B6184227B65B3D92C01A8724:DD1D004D80834448B276F125F8310F2A@zengwang.aoss.cn-sh-01.sensecoreapi-oss.cn/data/OVO-Bench
+
+/afs/zengwang/ads-cli sync \
+s3://196FFD00B6184227B65B3D92C01A8724:DD1D004D80834448B276F125F8310F2A@zengwang.aoss-internal.cn-sh-01.sensecoreapi-oss.cn/data/OVO-Bench \
+/afs/zengwang/projects/task_define_service/data/OVO-Bench
+
+
+unzip AutoEvalMetaData.zip
+unzip COIN.zip
+unzip Ego4D.zip
+unzip YouTube_Games.zip
+unzip cross_task-hirest-OpenEQA.zip
+unzip thumos.zip
+unzip youcook2.zip
+ls
