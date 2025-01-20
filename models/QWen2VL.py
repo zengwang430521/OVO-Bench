66d57fa3d2d8a4f12e13186fc8b32107bfdc3851
zengwang <zengwang@tetras.ai>
2025-01-20 17:11:52 +0800

    debug

--- models/QWen2VL.py
+++ models/QWen2VL.py
@@ -67,4 +67,6 @@ class EvalQWen2VL(OVOBenchOffline):
 
         outputs = self.llm.generate([llm_inputs], sampling_params=self.sampling_params)
         response = outputs[0].outputs[0].text
+        print(f"Prompt:\n{prompt}")
+        print(f"Response:\n{response}")
         return response
