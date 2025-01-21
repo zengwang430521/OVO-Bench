2652756c0cec1acd11205707f74bbf846cc026e4
zengwang <zengwang@tetras.ai>
2025-01-21 16:57:55 +0800

    debug

---
+++ eval_data.py
@@ -0,0 +1,13 @@
+import json
+from os.path import exists
+
+data_file = 'data/ovo_bench.json'
+with open(data_file, 'r') as f:
+    data = json.load(f)
+
+for item in data:
+    video_file = item['video']
+    task = item['task']
+    if not exists(video_file):
+        print(f"{task}: {video_file}")
+
