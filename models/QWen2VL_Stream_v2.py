adce94d58a87a58fbc570062c9e7cd91163ea0c7
zengwang <zengwang@tetras.ai>
2025-03-05 16:34:26 +0800

    debug

--- models/QWen2VL_Stream_v2.py
+++ models/QWen2VL_Stream_v2.py
@@ -43,6 +43,8 @@ class EvalQWen2VLStreamV2(OVOBenchOffline):
         )
 
         if lora_path is not None:
+            import pdb; pdb.set_trace()
+            print('Debug: load lora')
             model = PeftModel.from_pretrained(model, lora_path)
             model = model.merge_and_unload()
         model = model.eval()
