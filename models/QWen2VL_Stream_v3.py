825cf36478fa7c7b095868f22a9b81b6ab0f6c50
zengwang <zengwang@tetras.ai>
2025-03-17 17:52:59 +0800

    debug

--- models/QWen2VL_Stream_v3.py
+++ models/QWen2VL_Stream_v3.py
@@ -383,7 +383,7 @@ class EvalQWen2VLStreamV3Align(EvalQWen2VLStreamV2):
             check_times=None,
             only_one_response=False):
 
-        # import pdb; pdb.set_trace()
+        import pdb; pdb.set_trace()
 
         print(f"(Time: {query_time}) User:{prompt}")
         print(f'check times: {check_times}')
@@ -526,7 +526,7 @@ class EvalQWen2VLStreamV3Align(EvalQWen2VLStreamV2):
         past_key_values, rope_deltas = None, None
 
         def need_response():
-            import pdb; pdb.set_trace()
+            # import pdb; pdb.set_trace()
             nonlocal past_key_values, rope_deltas
             past_key_values, rope_deltas = None, None
 
